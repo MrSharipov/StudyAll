4326996861be4849b5a99573317660ad5d17ce40
MrSharipov <84614549+MrSharipov@users.noreply.github.com>
2023-07-13 06:53:44 +0500

    Factory DP is being studied

---
+++ Design Patterns/F.Dadajonov course/index.js
@@ -0,0 +1,24 @@
+/*
+******* Three types of DP ******
+
+- creational DP
+ Patterns to create an object
+
+- Structural
+ Patterns to organize and class and objects 
+
+- Behavioural
+ Pattern for communication between objects
+
+*/
+
+/*
+
+******* Simple Factory DP in OOP ******
+
+- The factory pattern is a creational design pattern that provides a generic interface for creating objects. In the factory pattern, we can specify the type of object being created and we do not need to explicitly require a constructor.
+
+- Object/Classses should be open for extension, but close for modificstion (SOLID)
+
+https://github.com/dFarkhod/DesignPatterns.SimpleFactory
+*/
